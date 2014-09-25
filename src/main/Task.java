1311dff16660a492a04404f2f2775239504f976a
slyj91 <slyj91@gmail.com>
2014-09-25 09:30:23 +0800

    Initial Task Object
    Basic structure of the task object

---
+++ src/main/Task.java
@@ -0,0 +1,45 @@
+package main;
+
+public class Task {
+	
+	private final static String TO_DO_LABEL = "To-Do";
+	
+	private String name;
+	private String description;
+	private String label;
+	private long timeStamp;
+	private long deadline;
+	
+	public Task(String name, String description) {
+		this(name, description, TO_DO_LABEL, -1);
+	}
+	
+	public Task(String name, String description, String label, long deadline) {
+		
+		this.name = name;
+		this.description = description;
+		this.label = label;
+		this.timeStamp = System.currentTimeMillis();
+		this.deadline = deadline;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public String getLabel() {
+		return label;
+	}
+	
+	public long getTimeStamp() {
+		return timeStamp;
+	}
+	
+	public long getDeadline() {
+		return deadline;
+	}
+}
