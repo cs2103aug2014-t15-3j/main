136d9909e1d489d7e31f33f418ace899f4e13464
plaknas <sankalp.s.plaknas@gmail.com>
2014-10-02 00:24:40 +0800

    Revert "Updated G"
    This reverts commit 8f6cf57143518237ab6d44eced67a2bcbcb1d89a.

--- GUI/GuiMain.java
+++ GUI/GuiMain.java
@@ -1,11 +1,12 @@
 /**
  * @author Sankalp 
  * 
+ * Dear Sankalp just incase you might misunderstood, you can definitely add in more files when needed
+ * no need to only code in this file :) 
+ * 
+ * Do remove this comment after you have seen it for acknowledgement - Candiie
+ *
  */
-
 public class GuiMain {
-	public GuiMain(){
-		
-		
-	}
+
 }
