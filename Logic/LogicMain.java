2d74445640e04b9461caf32757f0a0c05067fd64
slyj91 <slyj91@gmail.com>
2014-10-06 18:53:33 +0800

    LogicMain - Commit includes
    - processInput() will return a list of task(s) base on the input string
    
    - added "view" function in processInput()
    - - return a list with all the tasks
    
    - added "edit" function in processInput()
    - - return a list with the task that was edited
    
    - modified "add" function in processInput()
    - - return a list with the task that was added
    
    - modified "delete" function in processInput()
    - - return a list with the task that was deleted

--- Logic/LogicMain.java
+++ Logic/LogicMain.java
@@ -1,16 +1,19 @@
 //@author A0111942N
 
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Scanner;
-import java.util.TreeMap;
 
 public class LogicMain {
+	
+	private static boolean isInitialize = false;
+	
+	private static StorageMain storageMain;
 	
 	private static LinkedList<LogicInputPair> inputList;
 	private static LinkedList<Task> bufferList = new LinkedList<Task>();
 	
 	private static LinkedList<String> addOperations = OperationsConstant.getAddOperations();
+	private static LinkedList<String> nameOperations = OperationsConstant.getNameOperations();
 	private static LinkedList<String> descriptionOperations = OperationsConstant.getDescriptionOperations();
 	private static LinkedList<String> editOperations = OperationsConstant.getEditOperations();
 	private static LinkedList<String> viewOperations = OperationsConstant.getViewOperations();
@@ -20,43 +23,72 @@ public class LogicMain {
 	private static String[] inputArray;
 	
 	private static String MESSAGE_OPERATION_NOT_FOUND = "Remembra doesn't understand your input. Please try again..";
-
-	public static String processInput(String input) {
+	
+	public static LinkedList<Task> processInput(String input) {
 		
+		initialize();
+		
+		LinkedList<Task> packageTasks = new LinkedList<Task>();
 		input = cleanUpInput(input);
 		preProcessInput(input);
 		
 		String mainOperation = inputList.get(0).getOperation();
-		
+				
 		if( addOperations.contains(mainOperation) ) {
 			
-			if(executeAdd()) {
-				return OperationsConstant.ADD_OPERATION;
-			}
-			else {
-				return MESSAGE_OPERATION_NOT_FOUND;
-			}
-			
+			Task returnTask = new Task( executeAdd() );
+			returnTask.editState(OperationsConstant.ADD_OPERATION);
+			packageTasks.add(returnTask);
+			return packageTasks;
 		}
 		else if( editOperations.contains(mainOperation) ) {
 			
-			return OperationsConstant.EDIT_OPERATION;
+			Task returnTask = new Task( executeEdit() );
+			returnTask.editState(OperationsConstant.EDIT_OPERATION);
+			packageTasks.add(returnTask);
+			return packageTasks;
 		}
 		else if( viewOperations.contains(mainOperation) ) {
 			
-			return OperationsConstant.VIEW_OPERATION;
+			packageTasks = new LinkedList<Task>(bufferList);
+			
+			Task returnTask = new Task( packageTasks.get(0) );
+			returnTask.editState(OperationsConstant.VIEW_OPERATION);
+			
+			packageTasks.set(0, returnTask);
+			
+			return packageTasks;
 		}
 		else if( deleteOperations.contains(mainOperation) ) {
 			
-			return OperationsConstant.DELETE_OPERATION;
+			packageTasks.add(executeDelete());
+			return packageTasks;
 		}
 		else if( saveOperations.contains(mainOperation) ) {
 			
 			commitToStorage();
-			return OperationsConstant.SAVE_OPERATION;
+			return bufferList;
 		}
 		else {
-			return MESSAGE_OPERATION_NOT_FOUND;
+			return packageTasks;
+		}
+	}
+	
+	private static void initialize() {
+		
+		if(!isInitialize) {
+			storageMain = new StorageMain();
+			
+			Object retrievedObject = storageMain.retrieveObject(bufferList);
+			
+			if( retrievedObject instanceof LinkedList<?> ) {
+				bufferList = (LinkedList<Task>) retrievedObject;
+			}
+			else {
+				bufferList = new LinkedList<Task>();
+			}
+			
+			isInitialize = true;
 		}
 	}
 	
@@ -101,7 +133,7 @@ public class LogicMain {
 		}
 	}
 	
-	private static boolean executeAdd() {
+	private static Task executeAdd() {
 		
 		String name = "";
 		String description = "";
@@ -115,7 +147,7 @@ public class LogicMain {
 				name = inputList.get(i).getContent();
 				
 				if(name.isEmpty()) {
-					return false;
+					return null;
 				}
 			}
 			else if( descriptionOperations.contains(operation) ) {
@@ -124,8 +156,82 @@ public class LogicMain {
 			}
 		}
 		
-		bufferList.add( new Task(name,description) );
-		return true;
+		Task newTask = new Task(name,description);
+		bufferList.add(newTask);
+		return newTask;
+	}
+	
+	private static Task executeDelete() {
+		
+		int deleteID = 0;
+		String content = inputList.get(0).getContent();
+		
+		if(!content.isEmpty()) {
+			deleteID = Integer.parseInt(inputList.get(0).getContent());
+		}
+		
+		if(deleteID < bufferList.size()) {
+			Task deleteTask = bufferList.get(deleteID);
+			bufferList.remove(deleteID);
+			return deleteTask;
+		}
+		else {
+			return null;
+		}
+	}
+	
+	private static Task executeEdit() {
+		
+		int editID = -1;
+		String name = "";
+		String description = "";
+		boolean nameEdited = false;
+		boolean descriptionEdited = false;
+		
+		for(int i=0; i<inputList.size(); i++) {
+			
+			String operation = inputList.get(i).getOperation();
+			
+			if( editOperations.contains(operation) ) {
+				
+				String stringID = inputList.get(i).getContent();
+				
+				if(stringID.isEmpty()) {
+					return null;
+				}
+				
+				editID = Integer.parseInt(stringID);
+				
+				if(editID >= bufferList.size()) {
+					return null;
+				}
+			}
+			else if( nameOperations.contains(operation) ) {
+				
+				name = inputList.get(i).getContent();
+				
+				if(name.isEmpty()) {
+					return null;
+				}
+				
+				nameEdited = true;
+			}
+			else if( descriptionOperations.contains(operation) ) {
+				
+				description = inputList.get(i).getContent();
+				descriptionEdited = true;
+			}
+		}
+		
+		Task editTask = bufferList.get(editID);
+		
+		if(nameEdited) {
+			editTask.editName(name);
+		}
+		if(descriptionEdited) {
+			editTask.editDescription(description);
+		}
+		return editTask;
 	}
 	
 	private static boolean isOperation(String word) {
@@ -140,7 +246,6 @@ public class LogicMain {
 	
 	private static boolean commitToStorage() {
 		
-		StorageMain storageMain = new StorageMain();
 		storageMain.storeObject(bufferList);
 		return true;
 	}
