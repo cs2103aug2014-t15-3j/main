3cae3c1426d20839417c401f90b3547041c8c45d
Candiie <CandiieMusique@gmail.com>
2014-10-26 22:42:21 +0800

    Logic Commit - Added functions for LogicSearch.java:
    	Added - tokenizeSearchInput()
    	Filled in  - startLetterSearch()
    			   - matchWordSearch()
    			   - searchTasks()
    
    	*Considering to refactor code.

--- Logic/LogicSearch.java
+++ Logic/LogicSearch.java
@@ -8,7 +8,10 @@ import java.util.StringTokenizer;
 public final class LogicSearch {
 
 	//private Objects
-	private static LinkedList<String> suggestInputs = new LinkedList<String>(); //but if static can't be changed! ><" resolve this asap!
+	private LinkedList<String> suggestInputs = new LinkedList<String>(); //but if static can't be changed! ><" resolve this asap!
+	private LinkedList<String> tokenizedInputs = new LinkedList<String>(); 
+	private LinkedList<Task> matchedTasks = new LinkedList<Task>();
+	
 	
 	//@author A0112898U
 	/**
@@ -37,13 +40,196 @@ public final class LogicSearch {
 	
 	//@author A0112898U
 	/**
+	 * public overloaded method searchTasks() for 'Long' datatype query
+	 * for outside call to retrieve Stored Task 
+	 * 
+	 * @param searchType - the type of query type i.e. TYPE_DEADLINE/TYPE_TIME_ADDED
+	 * @param queryParam - query of 'Long' datatype variable is to input 
+	 * 
+	 * @return queryTasks returns a LinkedList with all task categorized with the 
+	 * same 'searchType' & 'Long' datatype variable
+	 * 
+	 */
+	private LinkedList<Task> searchTasks(SEARCH_TYPES searchType, long queryParam,  LinkedList<Task> bufferedTaskList){
+
+		LinkedList<Task> storedTasks = bufferedTaskList;
+		LinkedList<Task> tempCollatedList = new LinkedList();
+
+		switch(searchType){
+
+		case TYPE_DEADLINE:
+
+			for(Task t:storedTasks){
+
+				if(t.getDeadline() == queryParam){
+					tempCollatedList.add(t);
+				}
+			}
+
+			break;
+
+
+		case TYPE_TIME_ADDED:
+
+			for(Task t:storedTasks){
+
+				if(t.getTimeStamp() == queryParam){
+					tempCollatedList.add(t);
+				}
+			}
+
+			break;
+
+		default:
+			System.out.println("Type not supported");
+			return null;
+		}
+
+
+
+		return tempCollatedList;
+	}
+
+	//@author A0112898U
+	/**
+	 * public overloaded method searchTasks() for 'String' datatype query
+	 * for outside call to retrieve Stored Task 
+	 * 
+	 * @param searchType - the type of query type i.e. TYPE_NAME/TYPE_LABEL/TYPE_DESCRIPTION
+	 * @param queryParam - query of 'String' datatype variable is to input 
+	 * 
+	 * @return queryTasks returns a LinkedList with all task categorized with the 
+	 * same 'searchType' & 'String' datatype variable
+	 * 
+	 */
+	public LinkedList<Task> searchTasks(SEARCH_TYPES searchType, 
+					String queryString, LinkedList<Task> bufferedTaskList){
+
+		LinkedList<Task> storedTasks = bufferedTaskList;
+		LinkedList<Task> tempCollatedList = new LinkedList();
+		
+		//Tokenize the search input
+		tokenizedInputs = tokenizeSearchInput(queryString);
+
+		
+		switch(searchType){
+
+			case TYPE_NAME:
+				
+				//Search each query string through all the stored task via name
+				for (String searchString:tokenizedInputs){
+					
+					for(Task t:bufferedTaskList){ 
+						
+						if (t.getName().equals(searchString)){
+							
+							//If task not already added, add task to collated task
+							if(!tempCollatedList.contains(t)){
+								
+								//If task not already added, add task to collated task
+								if(!tempCollatedList.contains(t)){
+									
+									tempCollatedList.add(t);
+								}
+							}
+						}
+					}
+				}
+	
+				break;
+	
+			case TYPE_LABEL:
+				
+				//Search each query string through all the stored task via Label
+				for (String searchString:tokenizedInputs){
+					
+					for(Task t:bufferedTaskList){ 
+						
+						/* ask sam why label returns long
+						if (t.getLabel().equals(searchString)){
+						
+							//If task not already added, add task to collated task
+							if(!tempCollatedList.contains(t)){
+								
+								tempCollatedList.add(t);
+							}
+						}
+						*/
+					}
+				}
+	
+				break;
+				
+			case TYPE_DESCRIPTION:
+				
+				//Search each query string through all the stored task via name
+				for (String searchString:tokenizedInputs){
+					
+					for(Task t:bufferedTaskList){ 
+						
+						if (t.getDescription().contains(searchString)){
+							
+							tempCollatedList.add(t);
+						}
+					}
+				}
+	
+				//Finally return the collated list of matched task
+	
+				break;
+				
+			default:
+				
+				System.out.println("Type not supported");
+				return null;
+		}
+
+		return tempCollatedList;
+	}
+	
+	
+
+	//@author A0112898U
+	/**
+	 * Tokenizes the search input by user
+	 * 
+	 * @param searchString - search string input by user
+	 * 
+	 * @return a LinkedList of tokenized strings
+	 */
+	private static LinkedList<String> tokenizeSearchInput(String searchString){
+		
+		StringTokenizer strTokens = new StringTokenizer(searchString);
+		LinkedList<String> strToks = new LinkedList<String>();
+		
+		//Tokenizes the search String taken in
+		while (strTokens.hasMoreTokens()){
+			
+			strToks.add(strTokens.nextToken());
+		}
+		
+		return strToks;
+	}
+	
+	
+	//@author A0112898U
+	/**
 	 * Smart Searches for user input via Stored Tasks
+	 * 
+	 * @param collatedList - accepts a LinkedList<Task> type list that has been 
+	 * 						 previously initiated/accumulated 
+	 * 
+	 * @param bufferedTaskList - List of task that have been added by user
+	 * 
 	 */
 	public static void smartSearch(String searchLine, 
 				LinkedList<Task> listOfTasks, SEARCH_TYPES searchType){
 		
 		//Task that is collated through the searches.
 		LinkedList<Task> matchedTasks = new LinkedList<Task>(); 
+		
+		//Tokenized search inputs
+		
 		
 		switch(searchType){
 		
@@ -52,7 +238,6 @@ public final class LogicSearch {
 			case SEARCH_MATCH_WORD:
 			case SEARCH_POWER_SEARCH:
 				break;
-		
 		}
 		
 	}
@@ -72,14 +257,36 @@ public final class LogicSearch {
 	 * 
 	 * @return returns the newly collated list
 	 */
-	public static LinkedList<Task> startLetterSearch(LinkedList<Task> 
+	private LinkedList<Task> startLetterSearch(LinkedList<Task> 
 				collatedMatchedTaskList, LinkedList<Task> bufferedTaskList){
 		
 		LinkedList<Task> tempCollatedList = new LinkedList(collatedMatchedTaskList);
 		
-		//
-		//Breaks the task via tokenization method and search through
-		//
+		for (String searchString:tokenizedInputs){
+			
+			//if the token is a character
+			if (searchString.length() == 1){
+			
+				//Compare the token with the list of stored tasks
+				for (Task t:bufferedTaskList){ 
+				
+					//Tokenized the description of the tasks
+					LinkedList<String> desToks = tokenizeSearchInput(t.getDescription());
+				
+					//Search through the first Letter of every word in the description
+					for (String desTok:desToks){
+					
+						if (searchString.equals(Character.toString(desTok.charAt(0)))){
+							
+							if(!tempCollatedList.contains(t)){
+								
+								tempCollatedList.add(t);
+							}
+						}
+					}
+				}
+			}
+		}
 		
 		return tempCollatedList;
 		
@@ -99,14 +306,30 @@ public final class LogicSearch {
 	 * 
 	 * @return returns the newly collated list
 	 */
-	public static LinkedList<Task> matchWordSearch(LinkedList<Task> 
+	private LinkedList<Task> matchWordSearch(String queryString, LinkedList<Task> 
 				collatedMatchedTaskList, LinkedList<Task> bufferedTaskList){
 	
 		LinkedList<Task> tempCollatedList = new LinkedList(collatedMatchedTaskList);
 		
-		//
-		//Breaks the task via tokenization method and search through
-		//
+		//Tokenize the search input
+		tokenizedInputs = tokenizeSearchInput(queryString);
+		
+		//Search each query string through all the stored task via name
+		for (String searchString:tokenizedInputs){
+			
+			for(Task t:bufferedTaskList){ 
+				
+				if (t.getDescription().contains(searchString)){
+					
+					//If task not already added, add task to collated task
+					if(!tempCollatedList.contains(t)){
+						
+						tempCollatedList.add(t);
+					}
+				}
+			}
+		}
+
 		
 		return tempCollatedList;
 	}
@@ -118,14 +341,14 @@ public final class LogicSearch {
 	 * Simple substring search via .contains with the tokenized inputs
 	 * if applies, matched task is added to the list of return linkedlist string
 	 * 
-	 * @param collatedList - accepts a LinkedList<Task> type list that has been 
+	 * @param collatedMatchedTaskList - accepts a LinkedList<Task> type list that has been 
 	 * 						 previously initiated/accumulated 
 	 * 
 	 * @param bufferedTaskList - List of task that have been added by user
 	 * 
 	 * @return returns the newly collated list
 	 */
-	public static LinkedList<Task> subStringContainsSearch(LinkedList<Task> 
+	private static LinkedList<Task> subStringContainsSearch(LinkedList<Task> 
 				collatedMatchedTaskList, LinkedList<Task> bufferedTaskList){ 
 		
 		LinkedList<Task> tempCollatedList = new LinkedList(collatedMatchedTaskList);
@@ -143,120 +366,6 @@ public final class LogicSearch {
 		
 		return tempCollatedList;
 		
-	}
-	
-	
-	//@author A0112898U
-	/**
-	 * public overloaded method searchTasks() for 'Long' datatype query
-	 * for outside call to retrieve Stored Task 
-	 * 
-	 * @param searchType - the type of query type i.e. TYPE_DEADLINE/TYPE_TIME_ADDED
-	 * @param queryParam - query of 'Long' datatype variable is to input 
-	 * 
-	 * @return queryTasks returns a LinkedList with all task categorized with the 
-	 * same 'searchType' & 'Long' datatype variable
-	 * 
-	 */
-	public LinkedList<Task> searchTasks(SEARCH_TYPES searchType, long queryParam,  LinkedList<Task> bufferedTaskList){
-
-		LinkedList<Task> storedTasks = bufferedTaskList;
-		LinkedList<Task> queryTasks = new LinkedList<Task>();
-
-		switch(searchType){
-
-		case TYPE_DEADLINE:
-
-			for(Task t:storedTasks){
-
-				if(t.getDeadline() == queryParam){
-					queryTasks.add(t);
-				}
-			}
-
-			break;
-
-
-		case TYPE_TIME_ADDED:
-
-			for(Task t:storedTasks){
-
-				if(t.getTimeStamp() == queryParam){
-					queryTasks.add(t);
-				}
-			}
-
-			break;
-
-		default:
-			System.out.println("Type not supported");
-			return null;
-		}
-
-
-
-		return queryTasks;
-	}
-
-	//@author A0112898U
-	/**
-	 * public overloaded method searchTasks() for 'String' datatype query
-	 * for outside call to retrieve Stored Task 
-	 * 
-	 * @param searchType - the type of query type i.e. TYPE_NAME/TYPE_LABEL/TYPE_DESCRIPTION
-	 * @param queryParam - query of 'String' datatype variable is to input 
-	 * 
-	 * @return queryTasks returns a LinkedList with all task categorized with the 
-	 * same 'searchType' & 'String' datatype variable
-	 * 
-	 */
-	public LinkedList<Task> searchTasks(SEARCH_TYPES searchType, String queryParam, LinkedList<Task> bufferedTaskList){
-
-		LinkedList<Task> storedTasks = bufferedTaskList;
-		LinkedList<Task> queryTasks = new LinkedList<Task>();
-
-		switch(searchType){
-
-		case TYPE_NAME:
-
-			for(Task t:storedTasks){
-
-				if(t.getName() == queryParam){
-					queryTasks.add(t);
-				}
-			}
-
-			break;
-
-		case TYPE_LABEL:
-
-			for(Task t:storedTasks){
-				
-				//long labelID = getLabelId(queryParam); //Why need this call? TBA - ask sam
-				if(t.getLabel() == Long.parseLong(queryParam)){
-					queryTasks.add(t);
-				}
-			}
-
-			break;
-
-		case TYPE_DESCRIPTION:
-
-			for(Task t:storedTasks){
-
-				if(t.getDescription().contains(queryParam)){
-					queryTasks.add(t);
-				}
-			}
-
-			break;	
-
-		default:
-			System.out.println("Type not supported");
-			return null;
-		}
-
-		return queryTasks;
 	}
 	
 	
@@ -309,6 +418,7 @@ public final class LogicSearch {
 	 * is to be returned immediately, no need serach again :)
 	 * 
 	 */
+	/*
 	public static LinkedList<String> suggestSearchString(){
 		
 		//Exceptions is for users
@@ -316,5 +426,6 @@ public final class LogicSearch {
 		
 		return tempCollatedSuggestedStrings;
 	}
+	*/
 
 }
