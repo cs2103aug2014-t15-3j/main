ab080f635ff33b16a24f4c86a3411bfd7fc9cab0
Candiie <CandiieMusique@gmail.com>
2014-10-28 16:38:31 +0800

    Main Commit : Document examples for LogicSearch for LogicMain's implementation

--- Main/Main.java
+++ Main/Main.java
@@ -58,90 +58,62 @@ public class Main {
 		gui = new GuiMain();
 		gui.launch();
 		
+		StorageMain storageMain = new StorageMain();
+		
 				
-		/******************** Chuan Wei's Test Cases DO NOT REMOVE :D ******************************/
-		/******************************* STORAGE TEST **********************************************/	
-		
-		//Storage Creation (Only need to create once)
-		StorageMain storage = new StorageMain();
-		
-		/*
-		//Task Stubs for Storage
+		/************ Chuan Wei's Example for Logic Search for LogicMain Implementation ***********/
+		/*************************************** * DO NOT REMOVE :D *******************************/
+				
+		//Task Stubs for Logic Search
 		LinkedList<Task> taskList = new LinkedList<Task>();
 		LinkedList<Task> taskListStored = new LinkedList<Task>();
 		
-		Task task1 = new Task("task1","to love Remembra 1","lala",211014);
-		Task task2 = new Task("task2","to love Remembra 2","lala",211014);
-		Task task3 = new Task("task4","to love Remembra 1","lala",231014);
-		Task task4 = new Task("task3","to love Remembra 2","lala",241014);
+		Task task1 = new Task("task1","to love Remembra 1 uniqueKey1");
+		Task task2 = new Task("task2","to love Remembra 2 uniqueKey2");
+		Task task3 = new Task("task3","to love Remembra 1 uniqueKey3");
+		Task task4 = new Task("task4","to love Remembra 2 uniqueKey4");
+		Task task5 = new Task("task5","to love Remembra 0 Enchanted");
 		
 		taskList.add(task1);
 		taskList.add(task2);
 		taskList.add(task3);
 		taskList.add(task4);
+		taskList.add(task5);
 		
+		//retun all 
+		LinkedList<Task> searchedList1 = LogicSearch.searchTasks("love", taskList, 
+					LogicSearch.SEARCH_TYPES.TYPE_ALL);
 		
+		//return only 1 and 3
+		LinkedList<Task> searchedList2 = LogicSearch.searchTasks("task1 task4", taskList, 
+					LogicSearch.SEARCH_TYPES.TYPE_ALL);
 
+		//return task 1, 3, reason WHY task 4 didn't appear because standard search treats single character
+		//as the start of each word only, this reason is supported with the 'e' search in search list 5, 
+		//lets say if i search for e, i actually only want task 5 but all other task appears as a cluster
+		//it would be kind of non eye pleasing to the user
+		//
+		//So we must also indicate in the instructions that users can ask for advance search
+		//
+		LinkedList<Task> searchedList3 = LogicSearch.searchTasks("1 4 e", taskList, 
+					LogicSearch.SEARCH_TYPES.TYPE_ALL);
+
+		//return task 1, 3, 4
+		LinkedList<Task> searchedList4 = LogicSearch.searchTasks("1 4", taskList, 
+					LogicSearch.SEARCH_TYPES.TYPE_ALL, LogicSearch.SEARCH_TYPES.SEARCH_POWER_SEARCH);
+
+		//return all task
+		LinkedList<Task> searchedList5 = LogicSearch.searchTasks("1 4 e", taskList, 
+					LogicSearch.SEARCH_TYPES.TYPE_ALL, LogicSearch.SEARCH_TYPES.SEARCH_POWER_SEARCH);
 		
-		//Storage Call
-		storage.storeObject(taskList);
-		//storage.retrieveObject(taskListStored);	
 		
-		
-		//Test for search task by date
-		LinkedList<Task> _211014 = new LinkedList<Task>();
-		_211014 = storage.searchTask(true, false, "211014");
-		
-		System.out.println("By date Test");
-		for(Task t:_211014){
+		//Hi Sam, do change the index of the searchList'1|2|3|4' below to see the result
+		for(Task t:searchedList4){
 			System.out.println(t.getName());
 		}
 		
-		
-		//Test for search task by label
-		LinkedList<Task> labelList = new LinkedList<Task>();
-		labelList = storage.searchTask(false, true, "lala");
-
-		System.out.println("By label Test");
-		for(Task t:labelList){
-			System.out.println(t.getName());
-		}
-		
-		*/
-		
-		//Test for Label Storage
-		/*
-		LinkedList<Label> labels = new LinkedList<Label>();
-		
-		Label label1 = new Label("label2");
-		Label label2 = new Label("label5");
-		Label label3 = new Label("label3");
-		
-		labels.add(label1);
-		labels.add(label2);
-		labels.add(label3);
-		
-		storage.storeObject(StorageMain.OBJ_TYPES.TYPE_LABEL, labels);
-		
-		LinkedList<Label> rtvLabel = new LinkedList<Label>();
-		
-		rtvLabel = (LinkedList<Label>) storage.retrieveObject(StorageMain.OBJ_TYPES.TYPE_LABEL);
-		
-		System.out.println("I came here");
-		
-		for(Label ll: rtvLabel){
-			System.out.println(ll.getName());
-		}
-		
-		System.out.println("I came here");
-		*/
-		
-		//LinkedList<Label> rtvLabel = new LinkedList<Label>();
-		
-		//rtvLabel = (LinkedList<Label>) storage.retrieveObject(StorageMain.OBJ_TYPES.TYPE_LABEL);
-		
-		/******************************* STORAGE TEST **********************************************/
-		/******************** Chuan Wei's Test Cases DO NOT REMOVE :D ******************************/
+		/*************************************** * DO NOT REMOVE :D ******************************/
+		/************ Chuan Wei's Example for Logic Search for LogicMain Implementation ***********/	
 		
 	}
 
