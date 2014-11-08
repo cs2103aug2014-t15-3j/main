756cbbb9e2da4bd0d79e5da9b7f4ef5a6a47e7b3
Candiie <CandiieMusique@gmail.com>
2014-11-09 01:21:36 +0800

    ReminderTask Commit: Refactored to java coding standard

--- Objects/ReminderTask.java
+++ Objects/ReminderTask.java
@@ -3,88 +3,82 @@ import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
 
-class ReminderTask extends TimerTask{
+class ReminderTask extends TimerTask {
 	
+	//Class Objects
 	Task taskToRemind;
 	Timer taskAlarm;
 	Date alarmTime;
 	
-
 	
 	//@author A0112898U
 	/**
-	 * Constructor for Reminder Task
-	 * @param t
-	 * @param date
-	 * @throws ParseException
+	 * Constructor for Reminder Task.
+	 * 
+	 * @param inTask The task to use to construct a ReminderTask Object
+	 * @param inDate The reminder date of the task
 	 */
-	public ReminderTask(Task t, Date date) throws ParseException{
-
-		taskToRemind = t;
-		alarmTime = date;
+	public ReminderTask(Task inTask, Date inDate) {
+		taskToRemind = inTask;
+		alarmTime = inDate;
 	}
 	
 	
-	
 	//@author A0112898U
 	/**
+	 * Getter function to get the defined task.
 	 * 
 	 * @return the task for this ReminderTask
 	 */
-	public Task getTask(){
+	public Task getTask() {
 		return taskToRemind;
 	}
 	
 	
-	
 	//@author A0112898U
 	/**
+	 * API Function to edit a task
 	 * 
-	 * @return the task for this ReminderTask
+	 * @param editedTask The edited task to be updated to
 	 */
-	public void editTask(Task t){
-	
-		taskToRemind = new Task(t);
-		alarmTime = new Date(t.getReminder());
+	public void editTask(Task editedTask) {
+		taskToRemind = new Task(editedTask);
+		alarmTime = new Date(editedTask.getReminder());
 	}
 		
+	
+	//@author A0112898U
+	/**
+	 * Indicate the action to do when the alarm goes off 
+	 * for the scheduled reminder for this task
+	 * 
+	 * * This is a default function to have in order to be 
+	 * * scheduled.
+	 */
+	public void run() {		
+    	new Notification("Here's your reminder for : \n" + taskToRemind.getName(), 
+    			taskToRemind.getDescription(), 
+    			"Due on " + taskToRemind.getFormattedDeadline(), "", "").display();
+	}
 	
 	
 	//@author A0112898U
 	/**
-	 * Indicate the thing to do when the alarm goes off
+	 * Stops the alarm that have been scheduled.
 	 */
-	public void run() {
-	    
-    	/*System.out.println("Here's your reminder for : " 
-    			+ taskToRemind.getName()+ "\n" 
-    			+ taskToRemind.getDescription());
-    	*/
-		
-    	new Notification("Here's your reminder for : \n" + taskToRemind.getName()
-    			, taskToRemind.getDescription(), "Due on " + taskToRemind.getFormattedDeadline(), "", "").display();
-	}
-	
-	
-	
-	//@author A0112898U
-	/**
-	 * Stops the alarm that have been scheduled
-	 */
-	public void stopAlarm (){
-		
+	public void stopAlarm () {
 		taskAlarm.cancel();
 	}
 	
 	
-	
+	//@author A0112898U
 	/**
-	 * Schedules the alarm time for the task
-	 * @throws ParseException
+	 * Schedules the alarm time for the task.
+	 * 
+	 * @throws ParseException Throws exception if task can't be scheduled.
 	 */
 	public void scheduleAlarm () throws ParseException{
 		taskAlarm = new Timer();
-		//taskAlarm.schedule(new ReminderTask(t),  new Date(t.getReminder()));
 		taskAlarm.schedule(new ReminderTask(taskToRemind,alarmTime), alarmTime);
 	}
 }
